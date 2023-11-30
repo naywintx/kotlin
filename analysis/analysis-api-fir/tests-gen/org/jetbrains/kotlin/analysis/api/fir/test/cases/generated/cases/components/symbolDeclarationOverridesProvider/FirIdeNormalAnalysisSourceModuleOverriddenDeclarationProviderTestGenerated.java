2da688dcbbc24199a3848346404f651779a2df36
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2023-12-01 01:08:53 +0900

    [Analysis API] Fix overridden declaration computation for fake overrides
    According to the commit history, the previous behavior took place
    likely because of issues with implementing the override/implement
    dialog. However, it broke FIR implementations of
    'getAllOverriddenSymbols()' and 'getDirectlyOverriddenSymbols()' in case
    if a passed callable is a declaration-side substitution override.
    
    According to my tests, the mentioned functionality works as before
    after the change. In addition, there are several related tests in
    the IntelliJ Kotlin plugin that still pass:
    
    - FirOverrideImplementTest.testComplexMultiOverride()
    - FirOverrideImplementTest.testGenerateMulti()
    - FirOverrideImplementTest.testGenericClass()
    
    Note that in the current implementation, fake overrides are always
    unwrapped according to the KDoc of API methods.
    
    ^KT-62405 Fixed

--- analysis/analysis-api-fir/tests-gen/org/jetbrains/kotlin/analysis/api/fir/test/cases/generated/cases/components/symbolDeclarationOverridesProvider/FirIdeNormalAnalysisSourceModuleOverriddenDeclarationProviderTestGenerated.java
+++ analysis/analysis-api-fir/tests-gen/org/jetbrains/kotlin/analysis/api/fir/test/cases/generated/cases/components/symbolDeclarationOverridesProvider/FirIdeNormalAnalysisSourceModuleOverriddenDeclarationProviderTestGenerated.java
@@ -143,6 +143,48 @@ public class FirIdeNormalAnalysisSourceModuleOverriddenDeclarationProviderTestGe
     }
 
     @Test
+    @TestMetadata("substitutionOverrideFunctionParameter.kt")
+    public void testSubstitutionOverrideFunctionParameter() throws Exception {
+        runTest("analysis/analysis-api/testData/components/symbolDeclarationOverridesProvider/overriddenSymbols/substitutionOverrideFunctionParameter.kt");
+    }
+
+    @Test
+    @TestMetadata("substitutionOverrideFunctionReturnType.kt")
+    public void testSubstitutionOverrideFunctionReturnType() throws Exception {
+        runTest("analysis/analysis-api/testData/components/symbolDeclarationOverridesProvider/overriddenSymbols/substitutionOverrideFunctionReturnType.kt");
+    }
+
+    @Test
+    @TestMetadata("substitutionOverrideFunctionTransitive.kt")
+    public void testSubstitutionOverrideFunctionTransitive() throws Exception {
+        runTest("analysis/analysis-api/testData/components/symbolDeclarationOverridesProvider/overriddenSymbols/substitutionOverrideFunctionTransitive.kt");
+    }
+
+    @Test
+    @TestMetadata("substitutionOverrideFunctionTransitiveSameType.kt")
+    public void testSubstitutionOverrideFunctionTransitiveSameType() throws Exception {
+        runTest("analysis/analysis-api/testData/components/symbolDeclarationOverridesProvider/overriddenSymbols/substitutionOverrideFunctionTransitiveSameType.kt");
+    }
+
+    @Test
+    @TestMetadata("substitutionOverrideProperty.kt")
+    public void testSubstitutionOverrideProperty() throws Exception {
+        runTest("analysis/analysis-api/testData/components/symbolDeclarationOverridesProvider/overriddenSymbols/substitutionOverrideProperty.kt");
+    }
+
+    @Test
+    @TestMetadata("substitutionOverridePropertyTransitive.kt")
+    public void testSubstitutionOverridePropertyTransitive() throws Exception {
+        runTest("analysis/analysis-api/testData/components/symbolDeclarationOverridesProvider/overriddenSymbols/substitutionOverridePropertyTransitive.kt");
+    }
+
+    @Test
+    @TestMetadata("substitutionOverridePropertyTransitiveSameType.kt")
+    public void testSubstitutionOverridePropertyTransitiveSameType() throws Exception {
+        runTest("analysis/analysis-api/testData/components/symbolDeclarationOverridesProvider/overriddenSymbols/substitutionOverridePropertyTransitiveSameType.kt");
+    }
+
+    @Test
     @TestMetadata("substitutionOverrideWithImplicitTypeInsideAnonymousObject.kt")
     public void testSubstitutionOverrideWithImplicitTypeInsideAnonymousObject() throws Exception {
         runTest("analysis/analysis-api/testData/components/symbolDeclarationOverridesProvider/overriddenSymbols/substitutionOverrideWithImplicitTypeInsideAnonymousObject.kt");
