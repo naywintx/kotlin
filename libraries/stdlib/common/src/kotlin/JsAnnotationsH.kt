c805a67d9c0cf1bf90beb9c9018b70158aa24688
Ilya Gorbunov <ilya.a.gorbunov@gmail.com>
2024-01-28 21:00:37 +0000

    fixup! Fix compilation error

--- libraries/stdlib/common/src/kotlin/JsAnnotationsH.kt
+++ libraries/stdlib/common/src/kotlin/JsAnnotationsH.kt
@@ -163,8 +163,8 @@ public annotation class ExperimentalJsReflectionCreateInstance
 public annotation class ExperimentalJsCollectionsApi
 
 /**
- * Specifies that an additional static method needs to be generated from this element if it's a function.
- * If this element is a property, additional static getter/setter methods should be generated.
+ * Specifies that an additional static method is generated from the annotated member if it's a function.
+ * If the member is a property, additional static getter/setter methods are generated.
  */
 @ExperimentalJsStatic
 @Retention(AnnotationRetention.BINARY)
