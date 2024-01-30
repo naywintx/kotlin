dba8f70f4ae143bb7efb96402301346563c3d411
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2024-01-30 19:54:58 +0100

    [AA] Add code fragment session invalidation tests
    - These tests implement the module structure of most tests from the
      IDE's `DanglingFileModuleInvalidationTest`, allowing us to refactor
      most IDE-side invalidation tests into modification event tests.
    
    ^KT-56288

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/GlobalModuleStateModificationLLFirSessionInvalidationTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/sessions/GlobalModuleStateModificationLLFirSessionInvalidationTestGenerated.java
@@ -79,6 +79,54 @@ public class GlobalModuleStateModificationLLFirSessionInvalidationTestGenerated 
     }
 
     @Test
+    @TestMetadata("codeFragmentInvalidateFragment1.kt")
+    public void testCodeFragmentInvalidateFragment1() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/codeFragmentInvalidateFragment1.kt");
+    }
+
+    @Test
+    @TestMetadata("codeFragmentInvalidateFragment2.kt")
+    public void testCodeFragmentInvalidateFragment2() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/codeFragmentInvalidateFragment2.kt");
+    }
+
+    @Test
+    @TestMetadata("codeFragmentInvalidateLibrary.kt")
+    public void testCodeFragmentInvalidateLibrary() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/codeFragmentInvalidateLibrary.kt");
+    }
+
+    @Test
+    @TestMetadata("codeFragmentInvalidateNone.kt")
+    public void testCodeFragmentInvalidateNone() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/codeFragmentInvalidateNone.kt");
+    }
+
+    @Test
+    @TestMetadata("codeFragmentInvalidateSourceModule.kt")
+    public void testCodeFragmentInvalidateSourceModule() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/codeFragmentInvalidateSourceModule.kt");
+    }
+
+    @Test
+    @TestMetadata("codeFragmentWithContextModuleDependent.kt")
+    public void testCodeFragmentWithContextModuleDependent() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/codeFragmentWithContextModuleDependent.kt");
+    }
+
+    @Test
+    @TestMetadata("codeFragmentWithUnrelatedModule.kt")
+    public void testCodeFragmentWithUnrelatedModule() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/codeFragmentWithUnrelatedModule.kt");
+    }
+
+    @Test
+    @TestMetadata("codeFragmentWithUnrelatedModuleAndLibrary.kt")
+    public void testCodeFragmentWithUnrelatedModuleAndLibrary() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/codeFragmentWithUnrelatedModuleAndLibrary.kt");
+    }
+
+    @Test
     @TestMetadata("linearInvalidateC.kt")
     public void testLinearInvalidateC() throws Exception {
         runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/linearInvalidateC.kt");
@@ -136,5 +184,17 @@ public class GlobalModuleStateModificationLLFirSessionInvalidationTestGenerated 
     @TestMetadata("rhombusInvalidateLibrarySourceL2.kt")
     public void testRhombusInvalidateLibrarySourceL2() throws Exception {
         runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/rhombusInvalidateLibrarySourceL2.kt");
+    }
+
+    @Test
+    @TestMetadata("transitiveCodeFragment.kt")
+    public void testTransitiveCodeFragment() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/transitiveCodeFragment.kt");
+    }
+
+    @Test
+    @TestMetadata("unrelatedCodeFragment.kt")
+    public void testUnrelatedCodeFragment() throws Exception {
+        runTest("analysis/analysis-api/testData/sessions/sessionInvalidation/unrelatedCodeFragment.kt");
     }
 }
