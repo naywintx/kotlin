5344223fb6f74e5e630fe52e99ccab3dc715c15f
Nikita Bobko <nikita.bobko@jetbrains.com>
2024-02-15 18:56:45 +0100

    [FIR] Switch FirExpectActualDeclarationChecker to Platform checker
    ^KT-66028 Fixed

--- analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated.java
+++ analysis/low-level-api-fir/tests/org/jetbrains/kotlin/analysis/low/level/api/fir/diagnostic/compiler/based/LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated.java
@@ -26671,6 +26671,18 @@ public class LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated
       @TestDataPath("$PROJECT_ROOT")
       public class Hmpp {
         @Test
+        @TestMetadata("actualizeSubtypeBeforeSupertype.kt")
+        public void testActualizeSubtypeBeforeSupertype() {
+          runTest("compiler/testData/diagnostics/tests/multiplatform/hmpp/actualizeSubtypeBeforeSupertype.kt");
+        }
+
+        @Test
+        @TestMetadata("actualizedInPlatformSession.kt")
+        public void testActualizedInPlatformSession() {
+          runTest("compiler/testData/diagnostics/tests/multiplatform/hmpp/actualizedInPlatformSession.kt");
+        }
+
+        @Test
         public void testAllFilesPresentInHmpp() {
           KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler/testData/diagnostics/tests/multiplatform/hmpp"), Pattern.compile("^(.+)\\.(kt|kts)$"), Pattern.compile("^(.+)\\.(reversed|fir|ll)\\.kts?$"), true);
         }
@@ -26679,6 +26691,12 @@ public class LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated
         @TestMetadata("ambiguousActuals.kt")
         public void testAmbiguousActuals() {
           runTest("compiler/testData/diagnostics/tests/multiplatform/hmpp/ambiguousActuals.kt");
+        }
+
+        @Test
+        @TestMetadata("hmppModalityClash.kt")
+        public void testHmppModalityClash() {
+          runTest("compiler/testData/diagnostics/tests/multiplatform/hmpp/hmppModalityClash.kt");
         }
 
         @Test
@@ -26691,6 +26709,12 @@ public class LLFirPreresolvedReversedDiagnosticCompilerFE10TestDataTestGenerated
         @TestMetadata("hmppRedeclarationWithExpectActualPair.kt")
         public void testHmppRedeclarationWithExpectActualPair() {
           runTest("compiler/testData/diagnostics/tests/multiplatform/hmpp/hmppRedeclarationWithExpectActualPair.kt");
+        }
+
+        @Test
+        @TestMetadata("hmppSignatureClash.kt")
+        public void testHmppSignatureClash() {
+          runTest("compiler/testData/diagnostics/tests/multiplatform/hmpp/hmppSignatureClash.kt");
         }
 
         @Test
