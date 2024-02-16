4f62dc3258faef540f8ba9afb79dda2d1f3b75ce
Adam Semenenko <adam.semenenko@jetbrains.com>
2024-02-16 13:34:28 +0100

    fixup! Reimplement compileOnly dependencies check

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kmp-compileonly-dependency/settings.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/kmp-compileonly-dependency/settings.gradle.kts
@@ -1,8 +1,1 @@
 rootProject.name = "kmp-compileonly-dependency-multimodule"
-
-//include(":my-kmp-lib")
-include(":jvm-project")
-include(":native-project")
-
-//jsTarget: include(":js-project")
-//wasmTarget: include(":wasm-project")
