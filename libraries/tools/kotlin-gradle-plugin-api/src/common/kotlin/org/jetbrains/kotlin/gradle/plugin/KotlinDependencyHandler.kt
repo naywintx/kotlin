ffab57dc646423daeb5fe445256a11c7972e8e9c
Sarah Haggarty <Sarah.Haggarty@jetbrains.com>
2024-02-19 13:21:36 +0000

    fixup! [Gradle] Document KotlinDependencyHandler

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinDependencyHandler.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinDependencyHandler.kt
@@ -189,7 +189,7 @@ interface KotlinDependencyHandler : HasProject {
      * @see [HasKotlinDependencies.compileOnlyConfigurationName]
      *
      * @param dependency The dependency to add.
-     * @param configure additional configuration for the [dependency].
+     * @param configure Additional configuration for the [dependency].
      * @return The added [dependency].
      */
     fun <T : Dependency> compileOnly(dependency: T, configure: Action<T>) =
