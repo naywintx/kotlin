33bcae93b7457add80108d7f67ab9a08258c04b6
Sarah Haggarty <Sarah.Haggarty@jetbrains.com>
2024-02-19 13:22:22 +0000

    fixup! [Gradle] Document KotlinDependencyHandler

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinDependencyHandler.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinDependencyHandler.kt
@@ -219,7 +219,7 @@ interface KotlinDependencyHandler : HasProject {
     fun runtimeOnly(dependencyNotation: String, configure: ExternalModuleDependency.() -> Unit): ExternalModuleDependency
 
     /**
-     * Adds a runtime-only [module dependency](https://docs.gradle.org/current/userguide/declaring_dependencies.html#sub:module_dependencies)
+     * Adds a `runtimeOnly` [module dependency](https://docs.gradle.org/current/userguide/declaring_dependencies.html#sub:module_dependencies)
      * to this entity.
      *
      * @see [HasKotlinDependencies.runtimeOnlyConfigurationName]
