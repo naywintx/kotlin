02da7039bd3e1eb6771d1d20a6dc9f6fcbe7815f
Sarah Haggarty <Sarah.Haggarty@jetbrains.com>
2024-02-19 13:20:57 +0000

    fixup! [Gradle] Document KotlinDependencyHandler

--- libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinDependencyHandler.kt
+++ libraries/tools/kotlin-gradle-plugin-api/src/common/kotlin/org/jetbrains/kotlin/gradle/plugin/KotlinDependencyHandler.kt
@@ -160,7 +160,7 @@ interface KotlinDependencyHandler : HasProject {
     fun compileOnly(dependencyNotation: String, configure: ExternalModuleDependency.() -> Unit): ExternalModuleDependency
 
     /**
-     * Adds a compile-only [module dependency](https://docs.gradle.org/current/userguide/declaring_dependencies.html#sub:module_dependencies)
+     * Adds a `compileOnly` [module dependency](https://docs.gradle.org/current/userguide/declaring_dependencies.html#sub:module_dependencies)
      * to this entity.
      *
      * @see [HasKotlinDependencies.compileOnlyConfigurationName]
