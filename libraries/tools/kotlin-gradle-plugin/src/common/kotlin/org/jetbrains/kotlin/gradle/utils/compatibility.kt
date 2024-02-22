63827b73ffcad5b6006c69438b023540ca0d54c5
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2024-02-22 16:45:57 +0400

    Upgrade gradle version in tests

--- libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/compatibility.kt
+++ libraries/tools/kotlin-gradle-plugin/src/common/kotlin/org/jetbrains/kotlin/gradle/utils/compatibility.kt
@@ -31,7 +31,7 @@ import org.gradle.api.tasks.testing.TestDescriptor
 import org.gradle.util.GradleVersion
 import java.io.File
 
-const val minSupportedGradleVersion = "6.8.3"
+const val minSupportedGradleVersion = "7.4.2"
 
 internal fun checkGradleCompatibility(
     withComponent: String = "the Kotlin Gradle plugin",
