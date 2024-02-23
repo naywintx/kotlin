dc003853a891583368568731259fb1288e756fd7
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2024-02-23 13:42:31 +0100

    [IR] Drop IrIfThenElseImpl
    in order to simplify IR classes for future changes.
    
    IrIfThenElseImpl class was an additional implementation of IrWhen,
    besides the main IrWhenImpl. The only difference from IrWhenImpl
    was using another implementation of a list, which likely has little
    impact on performance. What's more, it is planned to replace all such
    lists with a new, custom implementation, at which point those classes
    would become identical.
    
    #KT-65773 Related

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrVisitor.kt
@@ -1658,13 +1658,13 @@ class Fir2IrVisitor(
                 }
             } else when (binaryLogicExpression.kind) {
                 LogicOperationKind.AND -> {
-                    IrIfThenElseImpl(startOffset, endOffset, irBuiltIns.booleanType, IrStatementOrigin.ANDAND).apply {
+                    IrWhenImpl(startOffset, endOffset, irBuiltIns.booleanType, IrStatementOrigin.ANDAND).apply {
                         branches.add(IrBranchImpl(leftOperand, rightOperand))
                         branches.add(elseBranch(constFalse(rightOperand.startOffset, rightOperand.endOffset)))
                     }
                 }
                 LogicOperationKind.OR -> {
-                    IrIfThenElseImpl(startOffset, endOffset, irBuiltIns.booleanType, IrStatementOrigin.OROR).apply {
+                    IrWhenImpl(startOffset, endOffset, irBuiltIns.booleanType, IrStatementOrigin.OROR).apply {
                         branches.add(IrBranchImpl(leftOperand, constTrue(leftOperand.startOffset, leftOperand.endOffset)))
                         branches.add(elseBranch(rightOperand))
                     }
