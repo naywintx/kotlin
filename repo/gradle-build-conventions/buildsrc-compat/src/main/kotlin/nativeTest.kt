fd54756139a5611c4b7f00dd95b2f1661fbeeaaa
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-02-23 16:14:48 +0100

    Revert "[REMOVE ME] Tweak coroutines I/O parallelism"
    This reverts commit fdb0d28ef3e0c7cb03bb08f89bb5548ae3f95bd1.

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/nativeTest.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/nativeTest.kt
@@ -130,7 +130,6 @@ fun Project.nativeTest(
             logger.info("$path JIT C2 compiler has been disabled")
             jvmArgs("-XX:TieredStopAtLevel=1") // Disable C2 if there are more than 4 CPUs at the host machine.
         }
-        systemProperty("kotlinx.coroutines.io.parallelism", 128)
 
         // Compute test properties in advance. Make sure that the necessary dependencies are settled.
         // But do not resolve any configurations until the execution phase.
