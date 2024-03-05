1ac9eeb742d945efb0577e3535b48ad6e0ab87b2
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2024-03-05 23:16:39 +0100

    [K/N] Remove more ext properties

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/BuildPropertiesExt.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/BuildPropertiesExt.kt
@@ -62,3 +62,6 @@ val KotlinBuildProperties.useFirTightIC: Boolean
 
 val KotlinBuildProperties.isSwiftExportPluginPublishingEnabled: Boolean
     get() = getBoolean("kotlin.native.swift-export.enabled", false)
+
+val KotlinBuildProperties.konanDataDir: String?
+    get() = getOrNull("konan.data.dir") as String?
