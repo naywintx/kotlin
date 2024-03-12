7d0ff66688da8cad7f94219cc834d4cb40f00e3f
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2024-03-12 18:31:37 +0100

    fixup! [K2] Add proper support for type operator evaluation

--- compiler/testData/diagnostics/tests/modifiers/const/typeOperator.kt
+++ compiler/testData/diagnostics/tests/modifiers/const/typeOperator.kt
@@ -9,6 +9,6 @@ const val asOperator2 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER!>1 <!CAST_NEVER_S
 <!TYPE_CANT_BE_USED_FOR_CONST_VAL!>const<!> val asOperator4 = "1" <!CAST_NEVER_SUCCEEDS!>as?<!> MyClass
 
 const val isOperator1 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER, USELESS_IS_CHECK!>1 is Int<!>
-const val isOperator2 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER!>1 is String<!>
-const val isOperator3 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER!>"1" !is Int<!>
-const val isOperator4 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER!>"1" !is MyClass<!>
+const val isOperator2 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER, USELESS_IS_CHECK!>1 is String<!>
+const val isOperator3 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER, USELESS_IS_CHECK!>"1" !is Int<!>
+const val isOperator4 = <!CONST_VAL_WITH_NON_CONST_INITIALIZER, USELESS_IS_CHECK!>"1" !is MyClass<!>
